9356e3c941ed736ff9fd5c894cbeea512c59f591
GissisBoras <145755974+GissisBoras@users.noreply.github.com>
2023-10-02 10:48:56 +0200

    Moved arround Simplyfied data Removed a lot of double data

--- Program.cs
+++ Program.cs
@@ -6,246 +6,87 @@ using System.Threading.Tasks;
 
 using System.Xml.Linq;
 
-using ConsoleApp1_230930;
-
-
-
-//namespace Class1; // Referera till product filen genom att lägga till ett using-direktiv
-//using AddProducts; // Referera till product filen genom att lägga till ett using-direktiv
-
-
-class Program
-
+namespace ConsoleApp1_230930
 {
-
-
-
-    static void Main()
+    class Program
 
     {
-        //Computer asset1 = new Computer("True", "Kalle", "Borås", "02-02-2023", "M121", "02-02-2026", "1234");
-        //Computer asset2 = new Computer(computerExists, nameOfUser, office, dateOfPurchase, modelName, endOfLifeDate, priceOfAsset);
-        //Computer asset3 = new Computer(computerExists, nameOfUser, office, dateOfPurchase, modelName, endOfLifeDate, priceOfAsset);
 
-        List<Tools> assetItems = new List<Tools>(); // List contining assets
 
-        //assetItem.Add.....
 
-        while (true)
+        static void Main()
+
         {
+            //Computer asset1 = new Computer("True" + "-" + "Kalle" + "-" + "Borås" + "-" + "02-02-2023" + "-" + "M121" + "-" + "02-02-2026" + "-" + "1234");
+            //Computer asset2 = new Computer(computerExists, nameOfUser, office, dateOfPurchase, modelName, endOfLifeDate, priceOfAsset);
+            //Computer asset3 = new Computer(computerExists, nameOfUser, office, dateOfPurchase, modelName, endOfLifeDate, priceOfAsset);
 
-            DisplayMenu();
+            List<Tools> assetItems = new List<Tools>(); // List contining assets
 
-            string choice = Console.ReadLine();
+            //assetItem.Add(asset1).....3St
 
-            switch (choice)
-            // Använder switch/case i stället för en if else loops
-            // för att hantera användarens val
-
+            while (true)
             {
 
-                case "1":
-                    {
-                        AddTool(assetItems); // Kalla på metod för att lägga till nytt verktyg
-                        break;
-                    }
+                DisplayMenu();
 
-                case "2":
-                    {
-                        
-                        ShowAssets(assetItems); // Kalla på metod för att visa (skriva ut) verkyg
-                        break;
-                    }
+                string choice = Console.ReadLine();
 
-                case "q":
+                switch (choice)
+                // Använder switch/case i stället för en if else loops
+                // för att hantera användarens val
 
-                    return; // Exit the program
-
-                default:
                 {
-                    Console.WriteLine("Invalid choice. Please try again.");
-                    break;
+
+                    case "1":
+                        {
+                            Tools.AddTool(assetItems); // Kalla på metod för att lägga till nytt verktyg
+                            break;
+                        }
+
+                    case "2":
+                        {
+
+                            Tools.ShowAssets(assetItems); // Kalla på metod för att visa (skriva ut) verkyg
+                            break;
+                        }
+
+                    case "q":
+
+                        return; // Exit the program
+
+                    default:
+                        {
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                        }
                 }
             }
-        }
 
 
-        static void DisplayMenu()
+            static void DisplayMenu()
 
-        {
-            //Skriv ut menyn
-
-            Console.WriteLine("\nMenu");
-            Console.WriteLine("\n====");
-            Console.WriteLine();
-            Console.WriteLine("1. Add asset");
-            Console.WriteLine("2. Show assets");
-           
-            Console.WriteLine("q. Quit");
-            Console.Write("Enter your choice: ");
-
-        }
-
-
-        static void ShowAssets(List<Tools> assetItems)
-        {
-            //Skriver ut listan 
-
-            Console.WriteLine("Sparade assets");
-            Console.WriteLine("==============\n");
-
-            foreach (Computer item in assetItems)
-            {     
-                Console.WriteLine($"{item.DateOfPurchase} : {item.Name} : {item.PriceOfAsset} : {item.Office}");
-            }
-
-            foreach (Phone item in assetItems)
             {
+                //Skriv ut menyn
 
-                Console.WriteLine($"{item.DateOfPurchase} : {item.Name} : {item.PriceOfAsset} : {item.Office}");
-            }
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("\n====");
+                Console.WriteLine();
+                Console.WriteLine("1. Add asset");
+                Console.WriteLine("2. Show assets");
 
-        }
-
-        static void AddTool(List<Tools> assetItems)     
-        {
-            Console.WriteLine("\nEnter asset computer(c) or phone(p):");
-
-            string assetType = Console.ReadLine();
-            assetType = assetType.ToLower().Trim();
-
-            if (assetType == "c")
-            {
-                //skapa nytt computer object i lista
-                addNewComputer(assetItems);
-            }
-            else if (assetType == "p")
-            {
-                //skapa nytt phone object i lista
-                AddnewPhpne(assetItems);
-            }
-            else
-            {
-                Console.WriteLine("error when selecting asset type");
-                //break;
+                Console.WriteLine("q. Quit");
+                Console.Write("Enter your choice: ");
 
             }
+
         }
 
     }
 
-    public static void addNewComputer(List<Tools> assetItems)
 
-    {
-
-       Console.Write("asset Name: ");
-       string AssetName = Console.ReadLine();
-        Console.Write("Office: ");
-        string office = Console.ReadLine();
-
-        Console.Write("Brand of computer: ");
-        string brandName = Console.ReadLine();
-
-        Console.Write("modellname: ");
-        string modelName = Console.ReadLine();
-
-        Console.Write("Datue (only date): ");
-        string Dday = Console.ReadLine();
-        Console.Write("Month (only month): ");
-        string DMonth = Console.ReadLine();
-        Console.Write("Year (only year 4 digits): ");
-        string Dyear = Console.ReadLine();
-
-        string dateOfPurchase = Dyear + "-" + DMonth + "-" + Dday;
-
-        int TempYear = Int32.Parse(Dyear);
-        TempYear = TempYear + 3;
-        string DTempYear = TempYear.ToString();
-
-        string endOfLifeDate = (DTempYear + "-" + DMonth + "-" + Dday); // Add 3 years to end of life for product
-
-        try
-        {
-
-            DateTime Test2 = Convert.ToDateTime(dateOfPurchase);
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Datumformat felaktigt");
-            throw;
-        }
-
-        Console.Write("Name of the person using the asset: ");
-        string nameOfUser = Console.ReadLine();
-
-        bool computerExists = true;
-        bool phoneExists = false;
-
-        Console.Write("Price: ");
-        double priceOfAsset = Convert.ToDouble(Console.ReadLine());
-
-
-        Computer asset = new Computer(computerExists, nameOfUser, office, dateOfPurchase, modelName, endOfLifeDate, priceOfAsset);
-
-        assetItems.Add(asset);
-        Console.WriteLine("Computer added successfully.");
-    }
-
-
-    public static void AddnewPhpne(List<Tools> assetItems)
-
-    {
-
-        Console.Write("asset Name: ");
-        string AssetName = Console.ReadLine();
-        Console.Write("Office: ");
-        string office = Console.ReadLine();
-
-        Console.Write("Brand of Phone: ");
-        string brandName = Console.ReadLine();
-
-        Console.Write("modellname: ");
-        string modelName = Console.ReadLine();
-
-        Console.Write("Datue (only date): ");
-        string Dday = Console.ReadLine();
-        Console.Write("Month (only month): ");
-        string DMonth = Console.ReadLine();
-        Console.Write("Year (only year 4 digits): ");
-        string Dyear = Console.ReadLine();
-
-        string dateOfPurchase = Dyear + "-" + DMonth + "-" + Dday;
-
-        int TempYear = Int32.Parse(Dyear);
-        TempYear = TempYear + 3;
-        string DTempYear = TempYear.ToString();
-
-        string endOfLifeDate = (DTempYear + "-" + DMonth + "-" + Dday); // Add 3 years to end of life for product
-
-        try
-        {
-
-            DateTime Test2 = Convert.ToDateTime(dateOfPurchase);
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Datumformat felaktigt");
-            throw;
-        }
-
-        Console.Write("Name of the person using the asset: ");
-        string nameOfUser = Console.ReadLine();
-
-        bool computerExists = true;
-        bool phoneExists = false;
-
-        Console.Write("Price: ");
-        double priceOfAsset = Convert.ToDouble(Console.ReadLine());
-
-        Phone asset = new Phone(phoneExists, nameOfUser, office, dateOfPurchase, modelName, endOfLifeDate, priceOfAsset);
-
-        assetItems.Add(asset);
-        Console.WriteLine("Computer added successfully.");
-    }
 }
+
+
+
 
--- Tools.cs
+++ Tools.cs
@@ -8,13 +8,81 @@ namespace ConsoleApp1_230930
 {
     internal class Tools
     {
-        public bool ComputerExists { get; set; }
-        public bool PhoneExists { get; set; }
-        public string Office { get; set; }
-        public string DateOfPurchase { get; set; }
-        public string Name { get; set; }
+        public Tools(string type, string brand, string modelName, string office, DateTime dateOfPurchase, double priceOfAsset)
+        {
+            Type = type;
+            Brand = brand;
+            ModelName = modelName;
+            Office = office;
+            DateOfPurchase = dateOfPurchase;
+            PriceOfAsset = priceOfAsset;
+        }
 
-        public string PriceOfAsset { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+
+        public string ModelName { get; set; }
+
+
+        public string Office { get; set; }
+
+        public DateTime DateOfPurchase { get; set; }
+        
+
+        public double PriceOfAsset { get; set; }
+
+
+        public static void AddTool(List<Tools> assetItems)
+
+        {
+
+            Console.Write("Type (computer or phone): ");
+            string type = Console.ReadLine();
+
+            Console.Write("Brand : ");
+            string brand = Console.ReadLine();
+
+            Console.Write("modellname: ");
+            string modelName = Console.ReadLine();
+
+            Console.Write("Office: ");
+            string office = Console.ReadLine();
+
+            Console.Write("inköpsdatum : ");
+            DateTime dateOfPurchase;
+            DateTime.TryParse(Console.ReadLine(),out dateOfPurchase );
+
+
+            Console.Write("Name of the person using the asset: ");
+            string nameOfUser = Console.ReadLine();
+
+            Console.Write("Price: ");
+            double priceOfAsset = Convert.ToDouble(Console.ReadLine());
+
+
+            Tools asset = new Tools(type, brand, modelName, office, dateOfPurchase, priceOfAsset);
+
+            assetItems.Add(asset);
+            Console.WriteLine("Computer added successfully.");
+        }
+
+        public static void ShowAssets(List<Tools> assetItems)
+        {
+            //Skriver ut listan 
+
+            Console.WriteLine("Sparade assets");
+            Console.WriteLine("==============\n");
+
+
+            foreach (Tools item in assetItems)
+            {
+
+                Console.WriteLine($"{item.DateOfPurchase} : {item.Brand} : {item.PriceOfAsset} : {item.Office}");
+            }
+
+        }
 
     }
+
+
 }
